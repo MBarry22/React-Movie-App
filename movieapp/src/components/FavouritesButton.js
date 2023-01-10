e0713a4007ade468304c08d753953a1af5a4cf2a
Mason Barry <mbarry22@my.bcit.ca>
2023-01-09 22:42:08 -0800

    Deploy

--- movieapp/src/components/FavouritesButton.js
+++ movieapp/src/components/FavouritesButton.js
@@ -11,7 +11,7 @@ export default function FavouritesButton(props) {
             let currMovies = JSON.parse(localStorage.getItem('movies'))
             
             
-            if(currmovies != null && currMovies[props.movieId.toString()]){
+            if(currMovies[props.movieId.toString()]){
                 setIsFavourites(true)
 
             }
